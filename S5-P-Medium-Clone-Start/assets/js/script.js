3b24c6f54e790196a12be157547060e1c6b6dae5
Giulius94 <giuliuscb@protonmail.com>
2023-11-28 16:31:30 +0100

    Animazioni M

--- S5-P-Medium-Clone-Start/assets/js/script.js
+++ S5-P-Medium-Clone-Start/assets/js/script.js
@@ -1,3 +1,4 @@
+// Scroll Animation
 document.addEventListener('scroll', () => {
     const header = document.querySelector('header');
     
@@ -7,3 +8,27 @@ document.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 })
+
+//Animazioni M
+let arrayG = document.querySelectorAll("g [opacity]")
+let n = document.querySelectorAll("g [opacity]").length;
+let casuale = 0;
+function giveMeRandom ()
+{
+    {
+        for(i = 0; i < n; i++)
+        {
+            casuale = Math.round(Math.random());
+            if(casuale === 0)
+            {
+                arrayG[i].classList.add("random");
+            }
+            else
+            {
+                arrayG[i].classList.remove("random");
+            }
+        }
+    }
+}
+giveMeRandom();
+setInterval(giveMeRandom, 500);
